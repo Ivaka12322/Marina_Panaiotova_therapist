d62ed1356bf5db19e9b71543261459127a47f49a
ElevateAI777 <adilazarov07@gmail.com>
2026-05-21 17:25:20 +0300

    Updated Books.tsx

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -69,7 +69,7 @@ export default function Books() {
               {/* Book image */}
               <div
                 className="relative overflow-hidden"
-                style={{ backgroundColor: '#F0E6D4', aspectRatio: '15/7' }}
+                style={{ backgroundColor: '#F0E6D4', aspectRatio: '13/7' }}
               >
                 <img
                   src={book.image}
